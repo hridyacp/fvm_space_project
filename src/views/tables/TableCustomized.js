e55e4e346aa9c95081d6a085370c48d8e9b08eda
hridyacp <jithridya@gmail.com>
2023-02-06 18:05:07 +0530

    Add table data

--- src/views/tables/TableCustomized.js
+++ src/views/tables/TableCustomized.js
@@ -44,32 +44,38 @@ const rows = [
 
 const TableCustomized = ({ orderData }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label='customized table'>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align='right'>Calories</StyledTableCell>
-            <StyledTableCell align='right'>Fat (g)</StyledTableCell>
-            <StyledTableCell align='right'>Carbs (g)</StyledTableCell>
-            <StyledTableCell align='right'>Protein (g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component='th' scope='row'>
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align='right'>{row.calories}</StyledTableCell>
-              <StyledTableCell align='right'>{row.fat}</StyledTableCell>
-              <StyledTableCell align='right'>{row.carbs}</StyledTableCell>
-              <StyledTableCell align='right'>{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {orderData ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                <StyledTableCell align='right'>Calories</StyledTableCell>
+                <StyledTableCell align='right'>Fat (g)</StyledTableCell>
+                <StyledTableCell align='right'>Carbs (g)</StyledTableCell>
+                <StyledTableCell align='right'>Protein (g)</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component='th' scope='row'>
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align='right'>{row.calories}</StyledTableCell>
+                  <StyledTableCell align='right'>{row.fat}</StyledTableCell>
+                  <StyledTableCell align='right'>{row.carbs}</StyledTableCell>
+                  <StyledTableCell align='right'>{row.protein}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        ''
+      )}
+    </>
   )
 }
 
